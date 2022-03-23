ddf41639635eccd5700e325b74ab930de3d6749b
alaiksandr kanavaliuk <aliaksandr_kanavaliuk@epam.com>
2022-03-23 15:53:13 +0100

    created DayDataRepository

---
+++ lib/data/repository/day_data_repositiry.dart
@@ -0,0 +1,14 @@
+import 'package:ak_test_app_1/data/api/api_util.dart';
+import 'package:ak_test_app_1/domain/model/day.dart';
+import 'package:ak_test_app_1/domain/repository/day_repository.dart';
+
+class DayDataRepository extends DayRepository {
+  final ApiUtil _apiUtil;
+
+  DayDataRepository(this._apiUtil);
+
+  @override
+  Future<Day> getDay({double latitude, double longtitude}) {
+    return _apiUtil.getDay(latitude: latitude, longitude: longitude);
+  }
+}
