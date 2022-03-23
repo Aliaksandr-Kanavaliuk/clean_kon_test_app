ea200c25ee6593752766913ebe61aee8e85e116d
alaiksandr kanavaliuk <aliaksandr_kanavaliuk@epam.com>
2022-03-23 14:54:24 +0100

    changed import

--- lib/domain/repository/day_repository.dart
+++ lib/domain/repository/day_repository.dart
@@ -1,4 +1,6 @@
-import 'package:ak_test_app_1/data/day.dart';
+// import 'package:ak_test_app_1/data/day.dart';
+
+import 'package:ak_test_app_1/domain/model/day.dart';
 
 abstract class DayRepository {
   Future<Day> getDay({
